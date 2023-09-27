608e4e1fa0b2b65fd112982dea5ed4fa9eed4f06
SamuelReginaldo <usr1@gmail.com>
2023-09-27 19:10:22 +0200

    eliminat Ranking i creat GestioRanking

---
+++ JuegoAhorcadoJava/src/main/GestioRanking.java
@@ -0,0 +1,22 @@
+
+package main;
+
+import java.util.Date;
+
+
+public class GestioRanking {
+    
+    private String rutaFitxerRanking;
+    private String nomJugador;
+    private int punts;
+    private Date data;
+    
+    public void afegirRanking( ){
+        
+        
+        
+    }
+    
+    
+            
+}
